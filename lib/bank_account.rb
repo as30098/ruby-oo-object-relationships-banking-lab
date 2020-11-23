691b6954d97163806a5fbec69c3ebeba5b491549
as30098 <alexspencer421@gmail.com>
2020-11-22 22:34:27 -0500

    commit tree 020cfef53165cb32d9e2c0cf24cb4bf9041b2f51

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,57 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    self.balance > 0 && self.status == "open"
+  end
+  #somewhere under the hood, balance and status will check for those variables within this class
 end
+
+# require 'pry'
+# class BankAccount
+
+#     attr_reader :name
+#     attr_accessor :balance, :status
+
+#     def initialize(name)
+#       @name = name
+#       @balance = 1000
+#       @status = "open"
+#     end
+
+#     def deposit(user_deposit)
+#       self.balance += user_deposit
+#     end
+
+#     def display_balance
+#       "Your balance is $#{self.balance}."
+#     end
+
+#     def valid?
+#       status == "open" && balance > 0
+#     end
+
+#     def close_account
+#         self.status = "closed"
+#     end
+
+# end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,96 @@
 class Transfer
-  # your code here
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
+# transaction1 = Transfer.new(Erwin, Alex, 30)
+# transaction1.execute_transaction
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
+
+
+# class Transfer
+
+
+#   attr_accessor :status, :amount, :sender, :receiver
+
+#   def initialize(sender, receiver, amount)
+#     @status = "pending"
+#     @sender = sender
+#     @receiver = receiver
+#     @amount = amount
+#   end
+
+#   def valid?
+#     sender.valid? && receiver.valid?
+#   # (sender.status == "open" && sender.balance > 0) && (receiver.status == "open" && receiver.balance > 0)
+#   # sender.valid? && receiver.valid?
+#   end
+
+#   def execute_transaction
+#     if valid? && sender.balance > amount && self.status == “pending”
+#       sender.balance -= amount
+#       receiver.balance += amount
+#       self.status = "complete"
+#     else
+#       reject_transfer
+#         #self.status = "rejected"
+#       # "Transaction rejected. Please check your account balance."
+#     end
+#   end
+
+#   def reverse_transfer
+#     if valid? && receiver.balance > amount && self.status == “complete”
+#       receiver.balance -= amount
+#       sender.balance += amount
+#       self.status = "reversed"
+#     else
+#       reject_transfer
+#       #self.status = "rejected"
+#       # "Transaction rejected. Please check your account balance."
+#     end
+#   end
+
+#   def reject_transfer
+#     self.status = "rejected"
+#     "Transaction rejected. Please check your account balance."
+#   end
+# end
+
+# # if sender.valid?
+# #   receiver.balance += amount
+
+# #   Edge case: if the sender does not have enough funds (does not have a valid account)
+# #     adding amount of sender to receiver balance
